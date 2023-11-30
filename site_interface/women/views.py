1bb1b9aec81d47dd2e61515829a7b7074dbb98bd
Eugeny1978 <luchnik78@mail.ru>
2023-12-01 00:02:28 +0300

    add

--- site_interface/women/views.py
+++ site_interface/women/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request): #HttpRequest
-    return HttpResponse('Страница Приложения women.')
+    return HttpResponse('Страница Приложения women.') #!
 
 def categories(request): #HttpRequest
     return HttpResponse('<h1>Статьи по Категориям</h1>')
