b3e2cc01ac1397b7c9f8cc8088ab777237d485ea
Eugeny1978 <luchnik78@mail.ru>
2023-12-05 12:31:04 +0300

    Modify files

--- site_interface/women/views.py
+++ site_interface/women/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpRespons
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify # фильтры
 
 # Create your views here.
 
@@ -12,23 +13,33 @@ class MyClass:
         self.a = a
         self.b = b
 
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+]
+
+
 def index(request): #HttpRequest
     # t = render_to_string('women/index.html')
     # return HttpResponse(t)
     data = {
-        'title': 'Главная Страница',
+        'title': 'главная страница',
         'menu': menu,
-        'float': 27.789,
-        'lst': [1, 4, 'asv', True],
-        'set': {1, 2, 5, 4, 2, 7},
-        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
-        'obj': MyClass(10, 20)
+        # 'main_title': 'главная страница',
+        # 'float': 27.789,
+        # 'lst': [1, 4, 'asv', True],
+        # 'set': {1, 2, 5, 4, 2, 7},
+        # 'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+        # 'obj': MyClass(10, 20),
+        # 'url': slugify('slugify The main page')
+        'posts': data_db
     }
     return render(request, 'women/index.html', context=data)
 
 def about(request):
     data = {
-        'title': 'О сайте',
+        'title': 'о сайте',
         'menu': menu
     }
     return render(request, 'women/about.html', context=data)
